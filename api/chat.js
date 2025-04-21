db68153e28eb332d0ce49a785db9e34f251706e7
beshN <beratshn2007@gmail.com>
2025-04-21 18:23:18 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -3,6 +3,7 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
+  // 🔥 Preflight CORS isteğini yakala
   if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
